870e72fe81bd9f4192b972a946875d3bd64facc7
Mohammad AlBaker Zaytoun <10119023@mu.edu.lb>
2022-11-13 01:08:48 +0200

    2nd commit: added encryption Fernit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pyasn1.compat.octets import null
 import time
 import calendar
 from tqdm import tqdm
+from cryptography.fernet import Fernet
 
 config = {
     "apiKey": "AIzaSyAx7NZxGLVxIprlcuAO_vglT0cWx9MXBJM",
@@ -24,6 +25,26 @@ config2 = {
     "appId": "1:82787672529:web:d739c74b00901d291cc0f0"
 }
 
+
+def genFKey():
+    key = Fernet.generate_key()
+    with open('mykey.key',
+              'wb') as mykey:  # Creates encryption key using Fernet algorithm for encryption and decryption
+        mykey.write(key)
+
+
+def EncryptFile(path, name):
+    with open('mykey.key', 'rb') as mykey:  # Reads encryption key
+        key = mykey.read()
+    f = Fernet(key)
+    with open(path, 'rb') as original_file:
+        original = original_file.read()
+    encryptedFile = f.encrypt(original)
+    EFName = "Encrypted " + name
+    with open(EFName, 'wb') as encryptedFileFile:
+        encryptedFileFile.write(encryptedFile)
+
+
 firebase2 = pyrebase.initialize_app(config2)
 database = firebase2.database()
 
@@ -34,6 +55,29 @@ isConnected = 0
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 storage = firebase.storage()
+
+count = database.child("logs").child("PC B").child("count").get()
+countD = count.val()
+
+for key, value in countD.items():
+    countL = value
+
+id = database.child("logs").child("PC B").child("id").get()
+idD = id.val()
+
+for key, value in idD.items():
+    id_AutoIncrement = value
+
+# countData = {
+#     "count": 1
+# }
+# database.child("logs").child("PC B").child("count").set(countData)
+
+# IDData = {
+#     "id": 1
+# }
+# database.child("logs").child("PC B").child("id").set(IDData)
+
 
 print("                                 Login\n")
 print("Email: ")
@@ -53,12 +97,13 @@ if firebase != null and firebase2 != null:
 timer = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 actionType = 3  # 3 is the default value
 time_stamp = calendar.timegm(current_GMT)
-id_AutoIncrement = -1
+
+anyString = "Log "
 
 if isConnected == 1:
     print("\n                            PC B: CONNECTED \n")
     print("Please choose your desired option: \n")
-    print("A- Upload Encrypted file \nB- Download Encrypted file\n")
+    print("A- Upload Encrypted file \nB- Download Encrypted file\nC- Exit")
     choice = input()
     if choice == 'A' or choice == 'a':
         print("Please insert the name of the file")
@@ -67,12 +112,14 @@ if isConnected == 1:
         pathL = input()
         pathU = pathL
         path_local = pathU
+        EncryptFile(path_local, name)
         print("Please insert the cloud address of the file you want to upload: ")
         pathCL = input()
         path_on_cloud = pathCL
         for i in tqdm(timer):
-            time.sleep(1)
-        storage.child(path_on_cloud).put(path_local)
+            time.sleep(3)
+        EFName = "Encrypted " + name
+        storage.child(path_on_cloud).put(EFName)
         id_AutoIncrement = id_AutoIncrement + 1
         actionType = 0
         time_stamp = calendar.timegm(current_GMT)
@@ -82,7 +129,10 @@ if isConnected == 1:
             "action_type": actionType,
             "filename": name
         }
-        database.child("logs").child("PC B").set(data)
+        database.child("logs").child("PC B").child("Log " + str(countL)).set(data)
+        countL = countL + 1
+        database.child("logs").child("PC B").child("count").update({"count": countL})
+        database.child("logs").child("PC B").child("id").update({"id": id_AutoIncrement})
 
 
     elif choice == 'B' or choice == 'b':
@@ -103,10 +153,16 @@ if isConnected == 1:
             "action_type": actionType,
             "filename": name
         }
-        database.child("logs").child("PC B").set(data)
+        database.child("logs").child("PC B").child("Log " + str(countL)).set(data)
+        countL = countL + 1
+        database.child("logs").child("PC B").child("count").update({"count": countL})
+        database.child("logs").child("PC B").child("id").update({"id": id_AutoIncrement})
+    elif choice == "C" or choice == "c":
+        exit()
 
     else:
         print("Invalid choice.")
+
 else:
     print("PC B: DISCONNECTED \n")
     print("CONNECTION FAILED")
